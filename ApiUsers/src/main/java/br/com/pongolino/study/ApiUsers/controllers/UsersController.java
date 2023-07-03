01af2e920091851bb995cf17a775c636c21ffdbb
victor.pongolino <victor.pongolino@hotmail.com>
2023-07-03 17:00:57 -0300

    Add Users microservice project

---
+++ ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/controllers/UsersController.java
@@ -0,0 +1,15 @@
+package br.com.pongolino.study.ApiUsers.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status/check")
+    public String health() {
+        return "Working";
+    }
+}
