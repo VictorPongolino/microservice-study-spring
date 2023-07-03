b931d6d7e54e092030a58325c8470c740f8cd123
victor.pongolino <victor.pongolino@hotmail.com>
2023-07-03 17:00:11 -0300

    Add Eureka Server to project

---
+++ AppDiscoveryService/src/main/java/br/com/pongolino/study/discovery/AppDiscoveryService/AppDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package br.com.pongolino.study.discovery.AppDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDiscoveryServiceApplication.class, args);
+	}
+
+}
