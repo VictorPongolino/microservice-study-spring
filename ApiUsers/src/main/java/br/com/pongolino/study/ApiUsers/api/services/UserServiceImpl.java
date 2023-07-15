39e4212c0502092f9c8f36d812e66598dccbc6ba
victor.pongolino <victor.pongolino@hotmail.com>
2023-07-14 22:33:34 -0300

    Fix circular dependency between service and

--- ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/api/services/UserServiceImpl.java
+++ ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/api/services/UserServiceImpl.java
@@ -8,6 +8,7 @@ import br.com.pongolino.study.ApiUsers.api.services.model.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
+    @Autowired @Lazy
     private PasswordEncoder passwordEncoder;
 
     @Override
