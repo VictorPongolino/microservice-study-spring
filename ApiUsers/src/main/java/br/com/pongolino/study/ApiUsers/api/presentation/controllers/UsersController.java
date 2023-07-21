af35f7d5cdb87e4f0e65f36a89da13f8fef4742c
victor.pongolino <victor.pongolino@hotmail.com>
2023-07-20 23:32:34 -0300

    Create a no arg constructor for UserCreationResponse and fixed mapping

--- ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/api/presentation/controllers/UsersController.java
+++ ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/api/presentation/controllers/UsersController.java
@@ -6,6 +6,7 @@ import br.com.pongolino.study.ApiUsers.api.services.UserService;
 import br.com.pongolino.study.ApiUsers.api.services.model.UserDto;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
@@ -28,7 +29,9 @@ public class UsersController {
     public ResponseEntity<UserCreationResponse> createUser(@RequestBody @Valid UserCreationRequest userCreationRequest) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        UserCreationResponse createdUser = userService.createUser(modelMapper.map(userCreationRequest, UserDto.class));
+        TypeMap<UserCreationRequest, UserDto> userDtoMapper = modelMapper.typeMap(UserCreationRequest.class, UserDto.class)
+                .addMappings(mapper -> mapper.map(UserCreationRequest::getPassword, UserDto::setRawPassword));
+        UserCreationResponse createdUser = userService.createUser(userDtoMapper.map(userCreationRequest));
 
         return ResponseEntity.status(HttpStatusCode.valueOf(204)).body(createdUser);
     }
--- ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/api/presentation/model/UserCreationResponse.java
+++ ApiUsers/src/main/java/br/com/pongolino/study/ApiUsers/api/presentation/model/UserCreationResponse.java
@@ -1,18 +1,21 @@
 package br.com.pongolino.study.ApiUsers.api.presentation.model;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Getter
 public class UserCreationResponse implements Serializable {
     @Serial
     private static final long serialVersionUID = 6466356877785163946L;
 
-    private final String fullName;
-    private final String email;
-    private final String password;
+    private String fullName;
+    private String email;
+    private String password;
 }
